70467ac59bb075c8984dd9c57a84914b2850bef8
vagrant <vagrant@precise64.(none)>
2013-08-12 21:06:40 -0700

    Tidying up a bit more

--- ruby-2.0.0/build/build-ruby2.0.0.sh
+++ ruby-2.0.0/build/build-ruby2.0.0.sh
@@ -15,11 +15,16 @@ ZLIB_VER=$(get_version zlib1g)
 LIBYAML_VER=$(get_version libyaml-0-2)
 OPENSSL_VER=$(get_version openssl)
 
-RUBY_VER_NUMBER=2.0.0
-RUBY_PATCH_VERSION=247
-RUBY_VER="ruby-$RUBY_VER_NUMBER"
-RUBY_VER_PATCH="$RUBY_VER_NUMBER-p$RUBY_PATCH_VERSION"
-TMP_DIR="/tmp/$RUBY_VER"
+RUBY_VER_MAJOR=2
+RUBY_VER_MINOR=0
+RUBY_VER_MAJMIN="$RUBY_VER_MAJOR.$RUBY_VER_MINOR"
+RUBY_VER_MIN=0
+RUBY_VER_NUMBER="$RUBY_VER_MAJMIN.$RUBY_VER_MIN"
+RUBY_VER_PATCH=247
+RUBY_VER="$RUBY_VER_NUMBER-p$RUBY_VER_PATCH"
+RUBY_VER_NAMED_STRING="ruby-$RUBY_VER_NUMBER"
+RUBY_VER_FULL_STRING="ruby-$RUBY_VER"
+TMP_DIR="/tmp/$RUBY_VER_NAMED_STRING"
 
 echo libstdc++6: $LIBSTDCPP_VER
 echo libc6: $LIBC_VER
@@ -32,11 +37,16 @@ echo zlib1g: $ZLIB_VER
 echo libyaml-0-2: $LIBYAML_VER
 echo openssl: $OPENSSL_VER
 
-cd "ruby-$RUBY_VER_PATCH"
+if [[ ! -a "$RUBY_VER_STRING.tar.gz" ]]; then
+	wget -c http://ftp.ruby-lang.org/pub/ruby/$RUBY_VER_MAJMIN/$RUBY_VER_FULL_STRING.tar.gz
+fi
+tar xzf $RUBY_VER_FULL_STRING.tar.gz
+
+cd $RUBY_VER_FULL_STRING
 rm -rf $TMP_DIR
 time (./configure --prefix=/usr && make && make install DESTDIR=$TMP_DIR )
-/usr/local/bin/fpm -s dir -t deb -n $RUBY_VER -v $RUBY_VER_PATCH --description "Self-packaged Ruby $RUBY_VER_NUMBER patch $RUBY_PATCH_VERSION" -C $TMP_DIR \
-  -p "$RUBY_VER"-VERSION_ARCH.deb -d "libstdc++6 (>= $LIBSTDCPP_VER)" \
+/usr/local/bin/fpm -s dir -t deb -n $RUBY_VER_NAMED_STRING -v $RUBY_VER --description "Self-packaged Ruby $RUBY_VER_NUMBER patch $RUBY_VER_PATCH" -C $TMP_DIR \
+  -p "$RUBY_VER_NAMED_STRING"-VERSION_ARCH.deb -d "libstdc++6 (>= $LIBSTDCPP_VER)" \
   -d "libc6 (>= $LIBC_VER)" -d "libffi6 (>= $LIBFFI_VER)" -d "libgdbm3 (>= $LIBGDBM_VER)" \
   -d "libncurses5 (>= $LIBNCURSES_VER)" -d "libreadline6 (>= $LIBREADLINE_VER)" \
   -d "libssl1.0.0 (>= $LIBSSL_VER)" -d "zlib1g (>= $ZLIB_VER)" \
@@ -44,5 +54,6 @@ time (./configure --prefix=/usr && make && make install DESTDIR=$TMP_DIR )
   usr/bin usr/lib usr/share/man usr/include
 
 cp *.deb ../
-rm *.deb
+cd ..
+rm -rf $RUBY_VER_FULL_STRING
 rm -rf /tmp/$RUBY_VER
